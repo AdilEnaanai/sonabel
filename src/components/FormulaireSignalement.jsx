0b274665c00952e9dfdbadc22f1693fdf00295ca
ENAANAI <aenaanai@uae.ac.ma>
2026-05-04 17:00:08 +0100

    Les états

--- src/components/FormulaireSignalement.jsx
+++ src/components/FormulaireSignalement.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import '../styles/FormulaireSignalement.css'
+import {CircleLoader} from 'react-spinners'
 const typesPannes = [
   "Coupure totale", "Basse tension", "Clignotement",
   "Compteur défectueux", "Autre"
@@ -10,36 +11,77 @@ export default function FormulaireSignalement() {
     nom: '',
     adresse: '',
     typePanne: typesPannes[0],
+    consommation: 0,
     description: ''
   })
+
+  const [etat, setEtat] = useState('pause')
+
+function valid(){
+    return !formData.nom || !formData.adresse || !formData.description || formData.consommation <= 0;
+}
+
+function handleChange(e){
+    const { name, value } = e.target;
+    setFormData(prevData => ({
+        ...prevData,
+        [name]: value
+    }))
+}
+function handleSubmit(e){
+    e.preventDefault();
+    setEtat('En cours')
+    setFormData({
+        nom: '',
+        adresse: '',
+        typePanne: typesPannes[0],
+        description: ''
+    })
+    setTimeout(() => {
+        setEtat('Envoyé')
+        setTimeout(() => setEtat('pause'), 5000)
+    }, 5000)
+    
+}
+
+function consommationFormat(){
+    let format={}
+    format['texte']=!formData.consommation ? '' : formData.consommation < 100 ? 'Consommation faible' : formData.consommation < 300 ? 'Consommation moyenne' : 'Consommation élevée'
+    format['couleur']=formData.consommation < 100 ? '#006633' : formData.consommation < 300 ? '#e68a00' : '#cc0000'
+    return format
+}
 
 return (
     <section className='formulaire-section'>
         <h2>Signaler une panne</h2>
-        <form className='formulaire-signalement'>
+        <CircleLoader loading={etat === 'En cours'} color="#e70000" size={30} />
+        <form className='formulaire-signalement' onSubmit={handleSubmit}>
             <label>
                 Nom :
                 <input
                     type="text"
+                    name="nom"
                     value={formData.nom}
-                    onChange={(e) => setFormData({ ...formData, nom: e.target.value })}
-                    required
+                    onChange={handleChange}
+                
                 />
             </label>
             <label>
                 Adresse :
                 <input
                     type="text"
+                    name="adresse"
                     value={formData.adresse}
-                    onChange={(e) => setFormData({ ...formData, adresse: e.target.value })}
-                    required
+                    onChange={handleChange}
+
                 />
             </label>
             <label>
                 Type de panne :
                 <select
                     value={formData.typePanne}
-                    onChange={(e) => setFormData({ ...formData, typePanne: e.target.value })}
+                    name='typePanne'
+                    onChange={handleChange}
                 >
                     {typesPannes.map((type) => (
                         <option key={type} value={type}>
@@ -49,14 +91,34 @@ return (
                 </select>
             </label>
             <label>
+                Consommation
+                <input
+                    type="number"
+                    name="consommation"
+                    value={formData.consommation || ''}
+                    onChange={handleChange}
+                />
+                <p style={{ color: consommationFormat().couleur }}>{consommationFormat().texte}</p>
+            </label>
+            <label>
                 Description :
                 <textarea
                     value={formData.description}
-                    onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-                    required
+                    name="description"
+                    onChange={handleChange}
                 />
             </label>
-            <button type="submit">Envoyer</button>
+            <button type="submit" disabled={valid()} >
+                Envoyer
+            </button>
         </form>
+        {
+            etat === 'Envoyé' && (
+                <div className="confirmation">
+                <h3>Signalement envoyé ✓</h3>
+                <p>Merci <strong>{formData.nom}</strong>, votre panne a été enregistrée.</p>
+                </div>
+            )
+        }
     </section>
 )}
